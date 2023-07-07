0416445a908f2d939de444dda11f23d0c41530aa
ISERP\MESA SOT <soutmesa3@gmail.com>
2023-07-07 15:33:52 +0700

    Added unit test for bad and not found request

---
+++ src/exception/not-found-error.exception.spec.ts
@@ -0,0 +1,19 @@
+import { BadRequestException } from '@nestjs/common';
+import { NotFoundErrorException } from './not-found-error.exception';
+
+describe('NotFoundErrorException', () => {
+  it('should extend BadRequestException', () => {
+    const options = {}; // Create options object as needed
+    const error = new NotFoundErrorException(options);
+
+    expect(error instanceof BadRequestException).toBe(true);
+  });
+
+  it('should set options correctly', () => {
+    const options = { message: 'Not found' }; // Create options object as needed
+    const error = new NotFoundErrorException(options);
+
+    expect(error.message).toBe(options.message);
+    // Add more assertions for other properties in the options object, if applicable
+  });
+});
